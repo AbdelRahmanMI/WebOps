d2bfde206ea8a6bc338f4d2190ef0e3035728f93
AbdelrahmanMohamed <abdelrahman.muhamedd@gmail.com>
2022-04-22 18:28:43 +0200

    modefing bootstrap , show users profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/users/sign_out" => "devise/sessions#destroy"
   end
+  get 'users/:id', to: 'users#show', as: 'profile'
   root 'home#index'
 end
