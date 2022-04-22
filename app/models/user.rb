dc356b74f608fb149762211884234b0c1b843898
AbdelrahmanMohamed <abdelrahman.muhamedd@gmail.com>
2022-04-22 21:26:10 +0200

    Post model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :posts
   has_one_attached :profile_picture
 
   #Validations
